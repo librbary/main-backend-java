b002afb7fa481850ca501d534fef73fd882a4efb
chandantiwari <chandan13tiwaris@gmail.com>
2023-03-28 21:08:22 +0530

    Added open api specifications

--- src/main/java/com/librbary/main/controller/UsersController.java
+++ src/main/java/com/librbary/main/controller/UsersController.java
@@ -1,6 +1,9 @@
 package com.librbary.main.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +11,14 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 @RestController
 @RequestMapping("/v1/users")
+@Tag(name = "liBRBary endpoints")
 public class UsersController {
   private static final Logger logger = getLogger(UsersController.class);
+
+  @Operation(summary = "Test endpoint")
+  @GetMapping(value = "/test")
+  public String getTest() {
+    return "Test";
+  }
 
 }
